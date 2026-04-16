f60684614692b40a131962964f0dadce2f3c918a
SimoAlbi28 (Simone Albini) <simone.albini28@gmail.com>
2026-04-16 19:07:19 +0200

    feat: update ESLint configuration and remove build ignore option from Next.js config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,13 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    rules: {
+      "react/jsx-no-comment-textnodes": "off",
+      "react-hooks/set-state-in-effect": "off",
+      "prefer-const": "warn",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
